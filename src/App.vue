ecc756536ec95055501ee33d3cbe546f9918dd65
Nick Weiland <nickweiland@gmail.com>
2018-03-21 21:58:19 -0700

    Implement vue

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <TodoList v-bind:todos="todos"></TodoList>
+    <CreateTodo v-on:createTodo="createTodo"></CreateTodo>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+
+export default {
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+  methods: {
+    createTodo(todo) {
+      this.todos.push({
+        ...todo,
+        done: false,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{todos.filter(todo => { return todo.done === true }).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => { return todo.done === false }).length}}</p>
+    <Todo
+      v-for="(todo, index) in todos"
+      :key="index"
+      v-bind:todo="todo"
+      v-on:deleteTodo="deleteTodo"
+      v-on:toggleTodo="toggleTodo">
+    </Todo>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+import Todo from './Todo';
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo,
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    toggleTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      const currentTodo = this.todos[todoIndex];
+      currentTodo.done = !currentTodo.done;
+    },
+  },
+};
+</script>
+<style>
+</style>
